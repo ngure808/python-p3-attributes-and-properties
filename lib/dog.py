257ac4e4d9a54f8e431b164a17c14e7861230ffa
ngure808 <mwangingure808@gmail.com>
2024-05-25 19:24:12 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,23 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed=None):
+        self.name = self.validate_string(name)
+        self.breed = self.validate_breed(breed) if breed is not None else None  
+
+    def validate_string(self, name):
+        if not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+            return None
+        if len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return None
+        return name
+    
+    def validate_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            return "Unknown"
+        else:
+            return breed
+
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,23 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="Person", job="Sales"):
+        self.name = self.validate_name(name)
+        self.job = self.validate_job(job)
+
+    def validate_name(self, name):
+        if not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+            return None
+        if len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return None
+        return name.title()  # Convert to title case
+
+    def validate_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            return None
+        return job
